45ece070a765757eeb5a2dca3657dc6516d2122a
PatrykDrozdPl <patryk.drozd85@gmail.com>
2023-01-24 18:49:49 +0100

    V2

---
+++ src/main/java/org/example/FoodFinder.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.List;
+
+public class FoodFinder {
+    Receipe createReceipe(List<String> ingredients) {
+
+        Receipe receipe = new Receipe("jajecznica");
+        return receipe;
+
+    }
+}
